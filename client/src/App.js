2768aeb9ad3d2976427972849662dd93fa02f614
Paul Miller <paul_john.miller0008@stud.hkr.se>
2022-03-22 14:50:16 +0100

    fixed errors in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,8 +17,6 @@ function App() {
     <div className='App'>
       <h1>React App</h1>
       <h2>{message}</h2>
-      <h3>State: {item}</h3>
-      <button onClick={handleClick}>Update state</button>
     </div>
   );
 }
